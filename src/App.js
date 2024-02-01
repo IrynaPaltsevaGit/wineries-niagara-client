7f4d0bcd38976a0f4609eedd35cbb4edaf43c470
IrynaPaltsevaGit <irynapaltseva@gmail.com>
2024-01-31 22:02:21 -0500

    connect with server done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [winneries, setWinneries] = useState([]);
+
+  useEffect(() => {
+    getData()
+  }, []);
+
+  async function getData() {
+    const response = await axios.get(`http://localhost:8080/wineries`);
+    if(response.data) {
+      setWinneries(response.data);
+    }
+    console.log(response.data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className='winneries'>
+      {
+        winneries.map((item, i) => {
+          return (
+            <li className='winneries__item' key={`item-${i}`}>
+              <div className='winneries__item-text'>
+                <p>{ item.title}</p>
+                <p> {item.address}</p>
+              </div>
+              <img className='winneries__img' src={item.image} />
+            </li>
+          );
+        })
+      }
+      </ul>
     </div>
   );
 }
