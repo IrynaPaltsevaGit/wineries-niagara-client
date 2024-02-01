76a366a476e0579d771630589029049b4aec4f8d
IrynaPaltsevaGit <irynapaltseva@gmail.com>
2024-02-01 17:46:27 -0500

    galina

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer class="footer">
     <div class="footer__logo">WINE</div>
-    <p class="footer__desk">Take a memorable detour for flavour...</p>
+    <p class="footer__desk">Hi! Take a memorable detour for flavour...</p>
     <p class="footer__terms">Copyright 2024 Багс & Дебагс, Inc. Terms & Privacy</p>
 </footer> 
   )
