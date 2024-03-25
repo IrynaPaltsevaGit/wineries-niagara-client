e2d17bf0490b355dc29a357a80bfd4fcbe15dc78
Galina Grogoryan <gala.april.ca@gmail.com>
2024-03-25 13:35:12 -0400

    first commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
     <footer class="footer">
-    <div class="footer__logo">WINE</div>
-    <p class="footer__desk">Take a memorable detour for flavour...</p>
-    <p class="footer__terms">Copyright 2024 Багс & Дебагс, Inc. Terms & Privacy</p>
-</footer> 
-  )
+      <div class="footer__logo">WINE</div>
+      <p class="footer__desk">Take a memorable detour for flavour!!!!!</p>
+      <p class="footer__terms">
+        Copyright 2024 Багс & Дебагс, Inc. Terms & Privacy
+      </p>
+    </footer>
+  );
 }
-
-
